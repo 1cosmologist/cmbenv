f8d3efb73dfaf526c659960b3cda2a7c3a2894a8
Shamik Ghosh <thequarkexpress@gmail.com>
2025-12-04 15:36:16 -0800

    Remove class

--- conf/make-pkgs.sh
+++ conf/make-pkgs.sh
@@ -1,7 +1,7 @@
 cd $CONDADIR
 mkdir aux
 cd aux
-git clone https://github.com/lesgourg/class_public.git class
-cd class
+# git clone https://github.com/lesgourg/class_public.git class
+# cd class
 make
 
